ff6ca20ffaf8f4da6c7577c7c75bb47c176837fb
helalmerchan <helalmerchan@gmail.com>
2024-09-02 19:33:33 +0600

    analylics

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { shadesOfPurple } from '@clerk/themes'
+import { Analytics } from "@vercel/analytics/react"
 
 // Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -18,6 +19,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     appearance={{baseTheme: shadesOfPurple}}
     publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <App />
+      <Analytics />
     </ClerkProvider>
+
   </React.StrictMode>,
 )
